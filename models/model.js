427dcc1c6210dc50082f316bf06847c8acd36d15
saeidsaeity <saeidsaeity@outlook.com>
2024-02-19 12:22:16 +0000

    adds /api/articles/:article_id endpoint

--- models/model.js
+++ models/model.js
@@ -1,6 +1,11 @@
 const db = require('../db/connection');
 const fs = require('fs/promises');
+    async function checkIfIdExists(table,id,key){
+        const {rows}= await db.query(`SELECT * FROM ${table} WHERE ${key} =  ${id} `)
+        return rows.length > 0
 
+
+    }
     async function selectTopics(){
        
         const {rows} = await db.query('SELECT * FROM topics ')
@@ -16,9 +21,19 @@ const fs = require('fs/promises');
         
 
     }
+   async function selectArticle(articleId){
+    if(await checkIfIdExists('articles',articleId,'article_id')){
+    const {rows} = await db.query('SELECT * FROM articles WHERE article_id = $1',[articleId])
+    return rows[0]
+    }
+    else{
+        return Promise.reject({status:404, msg: "Article Id does not exist"})
+    }
 
+   }
 
 module.exports = {
     selectTopics,
-    selectApi
+    selectApi,
+    selectArticle
 };
