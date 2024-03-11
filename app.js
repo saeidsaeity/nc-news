a0e0586b97eb18e40530f93f0d13860efaba80c7
saeidsaeity <saeidsaeity@outlook.com>
2024-03-11 09:03:46 +0000

    moves cors

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require("express");
 const apiRouter = require("./routers/api.router");
 const app = express();
 const cors = require('cors');
+app.use(cors())
+
 app.use(express.json());
 app.use("/api", apiRouter);
-app.use(cors())
 
 app.use((err, req, res, next) => {
   if (err.code === "42703" || err.code === "22P02" || err.code === '23502') {
